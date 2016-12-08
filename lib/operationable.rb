eaa773d09b59ab35af131c4cb8c3b5eca5e239a4
Kirill Suhodolov <kirillsuhodolov@gmail.com>
2016-12-08 17:38:49 +0700

    add files

--- lib/operationable.rb
+++ lib/operationable.rb
@@ -1,3 +1,6 @@
+require 'rails'
+require 'active_support/dependencies'
+
 require "operationable/version"
 
 require 'operationable/builder'
@@ -15,6 +18,11 @@ require 'operationable/specification'
 require 'operationable/runners/base'
 require 'operationable/runners/serial'
 require 'operationable/runners/separate'
+
+require '../app/jobs/operation_job'
+require '../app/operations/create'
+require '../app/operations/destroy'
+require '../app/operations/update'
 
 module Operationable
 
