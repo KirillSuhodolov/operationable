00f8ae3eb8f9906c10d3d21f1aad3273a06ab44c
Kirill Suhodolov <kirillsuhodolov@gmail.com>
2017-01-28 22:34:22 +0545

    pass user to specification

--- lib/operationable/specification.rb
+++ lib/operationable/specification.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 module Operationable
   class Specification
-    attr_reader :record, :params, :activity
+    attr_reader :record, :user, :params, :activity, :action_name
 
-    def initialize(record, params={}, activity='', action_name='')
+    def initialize(record, user, params={}, activity='', action_name='')
       @record = record
+      @user = user
       @params = params
       @activity = activity
       @action_name = action_name
--- lib/operationable/version.rb
+++ lib/operationable/version.rb
@@ -1,3 +1,3 @@
 module Operationable
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
