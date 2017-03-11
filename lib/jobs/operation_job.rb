179029cedc933e674a0843903af5ea931efb2b0b
KirillSuhodolov <kirillsuhodolov@gmail.com>
2017-03-11 02:54:26 +0200

    commit status

--- lib/jobs/operation_job.rb
+++ lib/jobs/operation_job.rb
@@ -7,7 +7,7 @@ class OperationJob < ActiveJob::Base
   end
 
   def perform(q_options:, props:)
-    "Operationable::Runners::#{q_options[:type].capitalize}".constantize.call(q_options, props)
+    "Operationable::Runners::#{q_options[:type].capitalize}".constantize.call(q_options: q_options, props: props)
   end
 
   after_perform do
