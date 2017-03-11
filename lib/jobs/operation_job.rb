dcaf06bae510d85af8f142263642505a22488270
KirillSuhodolov <kirillsuhodolov@gmail.com>
2017-03-11 02:32:21 +0200

    comment active job methods

--- lib/jobs/operation_job.rb
+++ lib/jobs/operation_job.rb
@@ -2,16 +2,16 @@
 class OperationJob
   include Resque::Plugins::Status
 
-  queue_as do
-    arguments.first[:queue]
-  end
+  # queue_as do
+  #   arguments.first[:queue]
+  # end
 
   def perform(options, props)
     "Operationable::Runners::#{options[:type].capitalize}".constantize.call(options, props)
   end
 
-  after_perform do
-    ActiveRecord::Base.clear_active_connections!
-    GC.start
-  end
+  # after_perform do
+  #   ActiveRecord::Base.clear_active_connections!
+  #   GC.start
+  # end
 end
