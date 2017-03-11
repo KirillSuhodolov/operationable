9241fc347a54dc4ba1ffa375f433dbd7912e2ba4
KirillSuhodolov <kirillsuhodolov@gmail.com>
2017-03-11 02:38:54 +0200

    comment active job methods

--- lib/jobs/operation_job.rb
+++ lib/jobs/operation_job.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
-class OperationJob
+class OperationJob < ActiveJob::Base
   include Resque::Plugins::Status
 
-  # queue_as do
-  #   arguments.first[:queue]
-  # end
-
-  def perform(options, props)
-    "Operationable::Runners::#{options[:type].capitalize}".constantize.call(options, props)
+  queue_as do
+    arguments.first[:queue]
   end
 
-  # after_perform do
-  #   ActiveRecord::Base.clear_active_connections!
-  #   GC.start
-  # end
+  def perform(q_options:, props:)
+    "Operationable::Runners::#{q_options[:type].capitalize}".constantize.call(q_options, props)
+  end
+
+  after_perform do
+    ActiveRecord::Base.clear_active_connections!
+    GC.start
+  end
 end
